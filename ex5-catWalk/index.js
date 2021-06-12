bbc6d75ecc9e4a2536c374e39e3a823d20f59b2e
Haleh zkr <haleh.zkr@gmail.com>
2021-06-12 17:15:46 +0430

    Update index.js

--- ex5-catWalk/index.js
+++ ex5-catWalk/index.js
@@ -45,7 +45,7 @@ function catWalk() {
       image.src = 'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
 
       setTimeout(function () {
-        image.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+        image.src = 'cat-walk.gif';
         moveTheCat = setInterval(catWalk, 50);
       }, 5000);
     }
